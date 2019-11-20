dbc3a7dbb217a6162a649760c68cf6f6aae4fcdc
Marco Wittig <marco.wittig@jambit.com>
2019-11-20 18:37:53 +0100

    Add FileReader for text files

---
+++ src/main/scala/org/sorted/chaos/utilities/FileReader.scala
@@ -0,0 +1,21 @@
+package org.sorted.chaos.utilities
+
+import org.slf4j.LoggerFactory
+
+import scala.io.Source
+import scala.util.{Failure, Success, Try}
+
+object FileReader {
+  private final val Log = LoggerFactory.getLogger(this.getClass)
+
+  def read(filename: String): Vector[String] =
+    Try(Source.fromResource(filename).getLines().toVector) match {
+      case Success(lines) => lines
+      case Failure(exception) =>
+        Log.error(
+          s"An error occurred during reading '$filename'.",
+          exception
+        )
+        Vector.empty
+    }
+}
