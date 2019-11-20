914965df128df2672d466e36bc2c5e134f728bcd
Marco Wittig <marco.wittig@jambit.com>
2019-11-20 21:07:25 +0100

    Extract methods to clean up code

--- src/main/scala/org/sorted/chaos/model/Wavefront.scala
+++ src/main/scala/org/sorted/chaos/model/Wavefront.scala
@@ -9,6 +9,10 @@ final case class Face(indexOfPoint1: Int, indexOfPoint2: Int, indexOfPoint3: Int
 final case class Wavefront(vertices: Vector[Point], faces: Vector[Face])
 
 object Wavefront {
+  private val Space = " "
+
+  private def empty = Wavefront(Vector.empty[Point], Vector.empty[Face])
+
   def from(filename: String): Wavefront = {
     val lines = FileReader.read(filename)
     lines.foldLeft(Wavefront.empty) { (accumulator, line) =>
@@ -16,19 +20,9 @@ object Wavefront {
         val parts = splitLine(line)
         parts(0) match {
           case "v" =>
-            val vertex = Point(
-              parts(1).toFloat,
-              parts(2).toFloat,
-              parts(3).toFloat
-            )
-            Wavefront(accumulator.vertices :+ vertex, accumulator.faces)
+            createVertex(accumulator, parts)
           case "f" =>
-            val face = Face(
-              parts(1).toInt - 1, // subtracted 1 because .obj count from 1 (not 0)
-              parts(2).toInt - 1,
-              parts(3).toInt - 1
-            )
-            Wavefront(accumulator.vertices, accumulator.faces :+ face)
+            createFace(accumulator, parts)
           case _ =>
             accumulator
         }
@@ -36,7 +30,27 @@ object Wavefront {
     }
   }
 
-  private def splitLine(line: String) = line.split(" ").toVector
+  private def splitLine(line: String) = line.split(Space).toVector
 
-  private def empty = Wavefront(Vector.empty[Point], Vector.empty[Face])
+  private def createVertex(accumulator: Wavefront, parts: Vector[String]) = {
+    val vertex = Point(
+      parts(1).toFloat,
+      parts(2).toFloat,
+      parts(3).toFloat
+    )
+    Wavefront(accumulator.vertices :+ vertex, accumulator.faces)
+  }
+
+  /**
+    * We subtract 1 from the index because .obj files are count from 1 but the index
+    * in the scala Vector data structure starts with 0
+    */
+  private def createFace(accumulator: Wavefront, parts: Vector[String]) = {
+    val face = Face(
+      parts(1).toInt - 1,
+      parts(2).toInt - 1,
+      parts(3).toInt - 1
+    )
+    Wavefront(accumulator.vertices, accumulator.faces :+ face)
+  }
 }
