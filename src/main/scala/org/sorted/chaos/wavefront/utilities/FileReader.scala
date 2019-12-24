0c379830e561dd3048f66accfc19fee3cb682ab6
Marco Wittig <marco.wittig@jambit.com>
2019-12-24 15:26:47 +0100

    Fix bug with path and filename when used as framework in another project
    The problem is the following... for reading a file from resources
    there are two options
    * Source.fromResource("myAwesome.txt")
    * getClass.getResourceAsStream("/myAwesome.txt")
    The problem is this missing "/" in front of the filename. To have things
    similar the FileReader had be modified. If you use Source.fromResource
    with "/" a NPE is thrown.

--- src/main/scala/org/sorted/chaos/wavefront/utilities/FileReader.scala
+++ src/main/scala/org/sorted/chaos/wavefront/utilities/FileReader.scala
@@ -8,14 +8,25 @@ import scala.util.{ Failure, Success, Try }
 object FileReader {
   private final val Log = LoggerFactory.getLogger(this.getClass)
 
-  def read(filename: String): Vector[String] =
-    Try(Source.fromResource(filename).getLines().toVector) match {
-      case Success(lines) => lines
+  def read(filename: String): Vector[String] = {
+    val maybeStream = Try(this.getClass.getResourceAsStream(filename))
+    maybeStream match {
       case Failure(exception) =>
         Log.error(
-          s"An error occurred during reading '$filename'.",
+          s"Can't create an InputStream from file: '$filename'. Exception was: ",
           exception
         )
-        Vector.empty
+        Vector.empty[String]
+      case Success(inStream) =>
+        Try(Source.fromInputStream(inStream).getLines().toVector) match {
+          case Success(lines) => lines
+          case Failure(exception) =>
+            Log.error(
+              s"An error occurred during reading '$filename'. Exception was: ",
+              exception
+            )
+            Vector.empty[String]
+        }
     }
+  }
 }
